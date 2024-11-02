76a83156c15f79fd222f393ecb488285da6a4c86
Tea-of-Austerity <teaofausterity@gmail.com>
2024-11-01 20:01:43 -0400

    solution for exercise 4

---
+++ exercise_4.rkt
@@ -0,0 +1,9 @@
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise-1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define str "helloworld")
+(define i 5)
+
+(define (replc str i) 
+    (string-append (substring str 0 (- i 1))  (substring str i (string-length str)))
+)
+
+(replc str i)
